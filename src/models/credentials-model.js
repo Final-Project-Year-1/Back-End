fa387123293b616c4952c8f1ccd6bfc4bc55c07b
ShirYahav <shiryahav07@gmail.com>
2024-06-23 18:00:12 +0300

    regex

--- src/models/credentials-model.js
+++ src/models/credentials-model.js
@@ -6,7 +6,7 @@ const CredentialsSchema = new Schema({
         required: [true, "Missing Email"],
         minlength: [5, "Email is too short"],
         maxlength: [150, "Email is too long"],
-        //match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Invalid Email"],
+        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Invalid Email"],
         trim: true,
         unique: true
     },
@@ -14,7 +14,7 @@ const CredentialsSchema = new Schema({
         type: String,
         required: [true, "Missing Password"],
         minlength: [8, "Password is too short"],
-        //match: [/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, "Password must contain at least one letter or number"],
+        match: [/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, "Password must contain at least one letter or number"],
         trim: true
     }
 }, {
--- src/models/user-model.js
+++ src/models/user-model.js
@@ -22,7 +22,7 @@ const UserSchema = new Schema({
         required: [true, "Missing Email"],
         minlength: [5, "Email is too short"],
         maxlength: [150, "Email is too long"],
-        //match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Invalid Email"],
+        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Invalid Email"],
         trim: true,
         unique: true
     },
@@ -30,7 +30,7 @@ const UserSchema = new Schema({
         type: String,
         required: [true, "Missing Password"],
         minlength: [8, "Password is too short"],
-        //match: [/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, "Password must contain at least one letter or number"],
+        match: [/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, "Password must contain at least one letter or number"],
         trim: true
     },
     role: {
