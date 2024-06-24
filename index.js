89d2b7eb62e90efd150b714b3ef98ba4ad611800
plasdje <omer30009@gmail.com>
2024-06-25 01:00:53 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dal from "./src/dal.js";
+const port = 3000;
+
+dal.connect();
+
+import authController from './src/controllers/auth-controller.js'
+import vacationController from './src/controllers/vacation-controller.js'
+import bookingController from './src/controllers/booking-controller.js'
+import reviewController from './src/controllers/review-controller.js'
+import numOfVacations from './src/controllers/vacations-number-logic.js'
+import numOfBooking from './src/controllers/booked-number-controller.js'
+const app = express();
+
+app.use(express.json())
+app.use("/api", authController);
+app.use("/api", vacationController);
+app.use("/api", bookingController);
+app.use("/api", reviewController);
+app.use("/api", numOfVacations);
+app.use("/api", numOfBooking);
+app.get('/', (req, res) => {
+  res.send('Welcome to my server!');
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
