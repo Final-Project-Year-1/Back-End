0af772ce6ab630a56b4ed52234754354594e07cd
plasdje <omer30009@gmail.com>
2024-08-01 16:24:15 +0300

    Update

--- src/models/vacation-model.js
+++ src/models/vacation-model.js
@@ -35,7 +35,7 @@ const VacationSchema = new Schema({
     },
     vacationType: {
         type: String,
-        enum: ['All-Inclusive', 'Bed and Breakfast', 'Full Board', 'Half Board','Nature','Culture','Adventure','Relaxation', 'Deleted'],
+        enum: ['All-Inclusive', 'Bed and Breakfast', 'FullBoard', 'HalfBoard', 'Cancelled'],
         required: true
     },
     companyName: {
