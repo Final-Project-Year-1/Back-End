fac9b9526d75621141ea2639224394fa419b29d4
plasdje <omer30009@gmail.com>
2024-08-02 17:06:11 +0300

    a

--- src/controllers/apiKey-controller.js
+++ src/controllers/apiKey-controller.js
@@ -32,35 +32,4 @@ router.get("/api-key/maps", async (request, response) => {
     }
 });
 
-router.get("/api-key/facebook", async (request, response) => {
-    try {
-        const apiKey = process.env.ACCESS_TOKEN
-        if(!apiKey){
-            return response.status(404).json('api key not found')
-        }
-
-        response.status(200).json(apiKey);
-    }
-    catch (err) {
-        console.log(err);
-        response.json(err);
-    }
-});
-
-router.get("/api-key/news", async (request, response) => {
-    try {
-        const apiKey = process.env.API_KEY_NEWS
-        if(!apiKey){
-            return response.status(404).json('api key not found')
-        }
-
-        response.status(200).json(apiKey);
-    }
-    catch (err) {
-        console.log(err);
-        response.json(err);
-    }
-});
-
-
-export default router
+export default router
