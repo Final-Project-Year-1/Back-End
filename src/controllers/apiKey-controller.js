a23ebbd14bc72adb8bbf1314e6dd878dce99c138
plasdje <omer30009@gmail.com>
2024-08-03 11:09:05 +0300

    ד

--- src/controllers/apiKey-controller.js
+++ src/controllers/apiKey-controller.js
@@ -32,4 +32,35 @@ router.get("/api-key/maps", async (request, response) => {
     }
 });
 
-export default router
+router.get("/api-key/facebook", async (request, response) => {
+    try {
+        const apiKey = process.env.ACCESS_TOKEN
+        if(!apiKey){
+            return response.status(404).json('api key not found')
+        }
+
+        response.status(200).json(apiKey);
+    }
+    catch (err) {
+        console.log(err);
+        response.json(err);
+    }
+});
+
+router.get("/api-key/news", async (request, response) => {
+    try {
+        const apiKey = process.env.API_KEY_NEWS
+        if(!apiKey){
+            return response.status(404).json('api key not found')
+        }
+
+        response.status(200).json(apiKey);
+    }
+    catch (err) {
+        console.log(err);
+        response.json(err);
+    }
+});
+
+
+export default router
